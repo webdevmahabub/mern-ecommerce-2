2f2c0a6be3b510088f6af7955b660dd68b5a0b5a
webdevmahabub <mahbubbd926@gmail.com>
2024-11-01 19:54:36 +0600

    Route And Login Register Setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+const Router = ({allRoutes}) => {
+    const routes = useRoutes([...allRoutes])
+    return routes;
+     
+};
+export default Router;
