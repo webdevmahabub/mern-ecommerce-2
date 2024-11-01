41b58e36701d1fbc784a3b7307e5dccfbd948f92
webdevmahabub <mahbubbd926@gmail.com>
2024-11-01 20:19:48 +0600

    Redux Toolkit Setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React,{lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
 const App = lazy(() => import('./App'))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Suspense>
+    <Provider store={store} >
       <App />
+      <Toaster
+      toastOptions={{
+        position : 'top-right',
+        style : {
+          background : '#283046',
+          color : 'white'
+        }
+      }} 
+    />
+      </Provider>
     </Suspense>
   </BrowserRouter>
 );
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
           <div className='w-[350px] text-[#ffffff] p-2'>
             <div className='bg-[#6f68d1] p-4 rounded-md'>
                 <h2 className='text-xl mb-3 font-bold'>Welcome to Ecommerce</h2>
-                <p className='text-sm mb-3 font-medium'>Please Sing In your account</p>
+                <p className='text-sm mb-3 font-medium'>Please Sign In your account</p>
     <form onSubmit={submit}>
          
         <div className='flex flex-col w-full gap-1 mb-3'>
